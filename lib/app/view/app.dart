92488b7f9304ae8471d2a96f96ff35c136cad8be
Clemens Schneider <johannes.clemens.schneider@gmail.com>
2024-06-16 16:06:38 +0200

    initial commit

---
+++ lib/app/view/app.dart
@@ -0,0 +1,36 @@
+import 'package:five_chat_app/l10n/l10n.dart';
+import 'package:five_chat_app/login/view/view.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+final GoRouter _router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return const LoginPage();
+      },
+    ),
+  ],
+);
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        ),
+        useMaterial3: true,
+      ),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      home: MaterialApp.router(
+        routerConfig: _router,
+      ),
+    );
+  }
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:five_chat_app/app/app.dart';
+import 'package:five_chat_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}

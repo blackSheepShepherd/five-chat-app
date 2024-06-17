ac3c454ec81f5a23e34b2346ae062e0c1d5b4a4d
Clemens Schneider <c.schneider.software@gmail.com>
2024-06-17 22:16:07 +0200

    feat: added supabase

---
+++ lib/main/environments.dart
@@ -0,0 +1,5 @@
+abstract class EnvironmentVariables {
+  static const String supabaseUrl = String.fromEnvironment('SUPABASE_URL');
+  static const String supabaseAnnonKey =
+      String.fromEnvironment('SUPABASE_ANNON_KEY');
+}
--- lib/main/main_staging.dart
+++ lib/main/main_staging.dart
@@ -1,6 +1,14 @@
 import 'package:five_chat_app/app/app.dart';
 import 'package:five_chat_app/main/bootstrap.dart';
+import 'package:five_chat_app/main/environments.dart';
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  bootstrap(() => const App());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: EnvironmentVariables.supabaseUrl,
+    anonKey: EnvironmentVariables.supabaseAnnonKey,
+  );
+  await bootstrap(() => const App());
 }
